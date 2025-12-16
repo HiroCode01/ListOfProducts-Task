7676449beec00a5e0e4da4380589e487ae66c7ec
Hiro <abdazimovfazliddin27@gmail.com>
2025-12-16 13:58:33 +0500

    Initial Commit

---
+++ ListOfProducts/ListOfProductsApp.swift
@@ -0,0 +1,18 @@
+//
+// ListOfProducts
+// ListOfProductsApp.swift
+//
+// Created by HiRO on 16/12/2025
+// Copyright © 2025 ACME. All Rights Reserved.
+
+
+import SwiftUI
+
+@main
+struct ListOfProductsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
